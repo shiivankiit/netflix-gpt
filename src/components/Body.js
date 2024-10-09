2765f8ffd8d5fdbab69bbcabd35a63e05120da7b
shiivankiit <singhankiit507@gmail.com>
2024-10-09 22:18:06 +0530

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,18 @@
+import Browse from "./Browse";
+import Login from "./Login";
+
+const Body = () => {
+// Here we create a routing over here.
+
+
+
+
+
+  return (
+  <div>
+    <Login/>
+    <Browse/>
+  </div>
+  );
+};
+export default Body;
